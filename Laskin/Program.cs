4033429c443723e5fd3819ee01ec46f6ed4ef0a6
varjeG <jenni.h.varis@gmail.com>
2020-09-24 08:13:43 +0300

    "Lisäsin kyselyn halutaanko ohjelma sulkea."

---
+++ Laskin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Laskin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervetuloa käyttämään laskinta.\n");
+        
+            while(true){
+                Console.WriteLine("Suoritetaan yhteenlasku.\n");
+                Console.WriteLine("Anna joku luku: ");
+
+                
+                
+                    int luku1= int.Parse(Console.ReadLine());
+                
+                    Console.WriteLine("Hyvä. Anna toinen luku: ");
+                    int luku2=int.Parse(Console.ReadLine());
+                    int tulos=luku1+luku2;
+
+                    Console.WriteLine(luku1+" + "+ luku2+" = "+ tulos);
+
+                    Console.WriteLine("Haluatko jatkaa ohjelman käyttöä? Y/N");
+                    string vastaus=Console.ReadLine();
+            
+                    if(vastaus=="N"||vastaus=="n"){
+                        break;
+                    }
+                }
+            
+            Console.WriteLine("Ohjelma on päättynyt. Näkemiin.");
+        }
+
+        }    
+    }
